288b00d900f1a8ce1335147869d5a20248c51bbf
2199671574@qq.com <2199671574@qq.com>
2024-11-24 17:09:42 +0800

    实验二第一个作业，线性布局

---
+++ app/src/main/java/com/example/proj_2_1/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.proj_2_1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.linearlayout)
+
+
+    }
+}
